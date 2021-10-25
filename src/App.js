e8a0bde94d10783e1030443d668fee1298d9cf90
unknown <mainulhasansadik@gmail.com>
2021-10-25 21:53:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import {GoogleAuthProvider,getAuth,signInWithPopup,signOut,FacebookAuthProvider } from 'firebase/auth';
+import {GithubAuthProvider} from 'firebase/auth'
+import { useState } from 'react';
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+
+
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
 function App() {
+  const [user,setUser] = useState({});
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const {displayName,email,photoURL} = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+      .catch(error => {
+      console.log(error.message);
+    })
+  }
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(loggedInUser);
+    })
+  }
+
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+      .then(result => {
+        console.log(result)
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(loggedInUser);
+      })
+      .catch(error => {
+      console.log(error.message);
+    })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {!user.name ?
+        <div>
+        <button onClick={handleGoogleSignIn}>Google Sign in</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+          <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
+      </div> :
+        <button onClick={handleSignOut}>Sign Out</button>
+      }
+      <br />
+      {
+        user.name && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email address : {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVl7BLeE-RRymCdDfIIu0d0N27_mxYNo0",
+    authDomain: "simple-firebase-auth-f3295.firebaseapp.com",
+    projectId: "simple-firebase-auth-f3295",
+    storageBucket: "simple-firebase-auth-f3295.appspot.com",
+    messagingSenderId: "230435914280",
+    appId: "1:230435914280:web:368aa38472c749a0caab1d"
+  };
+
+export default firebaseConfig;
